e839fa5fa93cc9443561a8baa490ca631aca8789
Marc Andrews <marc.andrews@rangle.io>
2017-12-19 22:45:48 -0500

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const isProdBuild = process.env.NODE_ENV === 'PRODUCTION';
+
+module.exports = {
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+    ],
+  },
+
+  entry: {
+    'dist/twitch-js': './src/index',
+  },
+
+  output: {
+    path: path.join(__dirname, './'),
+    filename: isProdBuild ? '[name].min.js' : '[name].js',
+    library: 'twitchJS',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+
+  plugins: isProdBuild ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false,
+      },
+    }),
+  ] : [],
+};
