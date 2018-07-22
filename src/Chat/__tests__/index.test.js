dd23234d3df298c464f3d481b502c2d249d76c6d
Marc Andrews <marcandrews@users.noreply.github.com>
2018-07-22 10:22:56 -0400

    Feature: improve ws mock (#108) [ci skip]
    * Improve ws mock

--- src/Chat/__tests__/index.test.js
+++ src/Chat/__tests__/index.test.js
@@ -1,4 +1,4 @@
-import WebSocket from 'ws'
+import { server } from 'ws'
 
 import commands from '../../../__mocks__/ws/__fixtures__/commands'
 import membership from '../../../__mocks__/ws/__fixtures__/membership'
@@ -8,11 +8,11 @@ import { onceResolve } from '../../utils'
 
 import Chat, { constants } from '../index'
 
+jest.mock('uws', () => require('ws'))
+
 describe('Chat', () => {
   let realDate
 
-  let wss
-  let server
   let chat
 
   const options = {
@@ -28,27 +28,17 @@ describe('Chat', () => {
     const DATE_TO_USE = new Date('2018')
     global.Date = jest.fn(() => DATE_TO_USE)
 
-    // Create WebSocket Server.
-    wss = new WebSocket.Server({ port: options.port })
-
-    const resolveOnConnection = new Promise(resolve => {
-      wss.on('connection', ws => {
-        server = ws
-        resolve()
-      })
-    })
-
     chat = new Chat(options)
-    return Promise.all([resolveOnConnection, chat.connect()])
+    return Promise.all([chat.connect()])
   })
 
   afterEach(() => {
+    // server.removeAllListeners()
     chat.removeAllListeners()
   })
 
-  afterAll(done => {
+  afterAll(() => {
     global.Date = realDate
-    wss.close(done)
   })
 
   test('should join channel', async () => {
@@ -57,144 +47,10 @@ describe('Chat', () => {
     expect(chat.channels).toEqual({ '#dallas': actual })
   })
 
-  test('should handle JOIN', done => {
-    chat.on(constants.EVENTS.JOIN, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(membership.JOIN)
-  })
-
-  test('should handle PART', done => {
-    chat.on(constants.EVENTS.PART, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(membership.PART)
-  })
-
-  test('should handle NAMES', async () => {
-    const emissions = Promise.all([
-      onceResolve(chat, constants.COMMANDS.NAMES),
-      onceResolve(chat, constants.COMMANDS.NAMES),
-      onceResolve(chat, constants.COMMANDS.NAMES_END),
-    ])
-
-    server.send(membership.NAMES)
-
-    return emissions.then(actual => expect(actual).toMatchSnapshot())
-  })
-
-  test('should handle MODE +o', done => {
-    chat.on(constants.EVENTS.MODE, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(membership.MODE.OPERATOR_PLUS)
-  })
-
-  test('should handle MODE -o', done => {
-    chat.on(constants.EVENTS.MODE, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(membership.MODE.OPERATOR_MINUS)
-  })
-
-  test('should handle CLEARCHAT', done => {
-    chat.on(constants.EVENTS.CLEAR_CHAT, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(commands.CLEARCHAT.CHANNEL)
-  })
-
-  test('should handle CLEARCHAT user with reason', done => {
-    chat.on(constants.EVENTS.CLEAR_CHAT, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(commands.CLEARCHAT.USER_WITH_REASON)
-  })
-
-  test('should handle HOSTTARGET start', done => {
-    chat.on(constants.EVENTS.HOST_TARGET, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(commands.HOSTTARGET.START)
-  })
-
-  test('should handle HOSTTARGET stop', done => {
-    chat.on(constants.EVENTS.HOST_TARGET, message => {
-      expect(message).toMatchSnapshot()
-      done()
-    })
-
-    server.send(commands.HOSTTARGET.STOP)
-  })
-
-  describe('should handle NOTICE', () => {
-    test.each(Object.entries(commands.NOTICE))(
-      '%s',
-      (name, raw, done) => {
-        chat.on(constants.EVENTS.NOTICE, message => {
-          expect(message).toMatchSnapshot()
-          done()
-        })
-
-        server.send(raw)
-      },
-      5000,
-    )
-  })
-
-  describe('should handle USERNOTICE', () => {
-    test.each(Object.entries(tags.USERNOTICE))('%s', (name, raw, done) => {
-      chat.on(constants.COMMANDS.USER_NOTICE, message => {
-        expect(message).toMatchSnapshot()
-        done()
-      })
-
-      server.send(raw)
-    })
-  })
-
-  describe('should handle PRIVMSG', () => {
-    test('should emit PRIVMSG', done => {
-      expect.assertions(1)
-
-      chat.on('PRIVMSG', actual => {
-        expect(actual).toMatchSnapshot()
-        done()
-      })
-
-      server.send(tags.PRIVMSG.NON_BITS)
-    })
-
-    test('should emit PRIVMSG with bits', done => {
-      expect.assertions(1)
-
-      chat.on('PRIVMSG', actual => {
-        expect(actual).toMatchSnapshot()
-        done()
-      })
-
-      server.send(tags.PRIVMSG.BITS)
-    })
-  })
-
   test('should send message to channel', done => {
     expect.assertions(1)
 
-    server.on('message', message => {
+    server.once('message', message => {
       expect(message).toEqual('PRIVMSG #dallas :Kappa Keepo Kappa')
       done()
     })
@@ -202,17 +58,164 @@ describe('Chat', () => {
     chat.say('#dallas', 'Kappa Keepo Kappa')
   })
 
-  describe('should handle deviations', () => {
-    test('CLEARCHAT deviation 1', done => {
-      expect.assertions(1)
-
-      chat.on('CLEARCHAT', actual => {
-        expect(actual).toMatchSnapshot()
+  describe('should handle messages', () => {
+    test('JOIN', done => {
+      chat.once(constants.EVENTS.JOIN, message => {
+        expect(message).toMatchSnapshot()
         done()
       })
 
-      server.send(commands.CLEARCHAT.DEVIATION_1)
+      server.sendMessageToClient(membership.JOIN)
     })
+
+    test('PART', done => {
+      chat.once(constants.EVENTS.PART, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(membership.PART)
+    })
+
+    test('NAMES', async () => {
+      const emissions = Promise.all([
+        onceResolve(chat, constants.COMMANDS.NAMES),
+        onceResolve(chat, constants.COMMANDS.NAMES),
+        onceResolve(chat, constants.COMMANDS.NAMES_END),
+      ])
+
+      server.sendMessageToClient(membership.NAMES)
+
+      return emissions.then(actual => expect(actual).toMatchSnapshot())
+    })
+
+    test('MODE +o', done => {
+      chat.once(constants.EVENTS.MODE, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(membership.MODE.OPERATOR_PLUS)
+    })
+
+    test('MODE -o', done => {
+      chat.once(constants.EVENTS.MODE, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(membership.MODE.OPERATOR_MINUS)
+    })
+
+    test('CLEARCHAT', done => {
+      chat.once(constants.EVENTS.CLEAR_CHAT, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(commands.CLEARCHAT.CHANNEL)
+    })
+
+    test('CLEARCHAT user with reason', done => {
+      chat.once(constants.EVENTS.CLEAR_CHAT, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(commands.CLEARCHAT.USER_WITH_REASON)
+    })
+
+    test('HOSTTARGET start', done => {
+      chat.once(constants.EVENTS.HOST_TARGET, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(commands.HOSTTARGET.START)
+    })
+
+    test('HOSTTARGET stop', done => {
+      chat.once(constants.EVENTS.HOST_TARGET, message => {
+        expect(message).toMatchSnapshot()
+        done()
+      })
+
+      server.sendMessageToClient(commands.HOSTTARGET.STOP)
+    })
+
+    describe('NOTICE', () => {
+      test.each(Object.entries(commands.NOTICE))(
+        '%s',
+        (name, raw, done) => {
+          chat.once(constants.EVENTS.NOTICE, message => {
+            expect(message).toMatchSnapshot()
+            done()
+          })
+
+          server.sendMessageToClient(raw)
+        },
+        5000,
+      )
+    })
+
+    describe('USERNOTICE', () => {
+      test.each(Object.entries(tags.USERNOTICE))('%s', (name, raw, done) => {
+        chat.once(constants.COMMANDS.USER_NOTICE, message => {
+          expect(message).toMatchSnapshot()
+          done()
+        })
+
+        server.sendMessageToClient(raw)
+      })
+    })
+
+    describe('PRIVMSG', () => {
+      test('PRIVMSG', done => {
+        expect.assertions(1)
+
+        chat.on('PRIVMSG', actual => {
+          expect(actual).toMatchSnapshot()
+          done()
+        })
+
+        server.sendMessageToClient(tags.PRIVMSG.NON_BITS)
+      })
+
+      test('CHEER', done => {
+        expect.assertions(1)
+
+        chat.on('PRIVMSG', actual => {
+          expect(actual).toMatchSnapshot()
+          done()
+        })
+
+        server.sendMessageToClient(tags.PRIVMSG.BITS)
+      })
+    })
+
+    describe('deviations', () => {
+      test('CLEARCHAT deviation 1', done => {
+        expect.assertions(1)
+
+        chat.on('CLEARCHAT', actual => {
+          expect(actual).toMatchSnapshot()
+          done()
+        })
+
+        server.sendMessageToClient(commands.CLEARCHAT.DEVIATION_1)
+      })
+    })
+  })
+
+  test('should part a channel', done => {
+    expect.assertions(1)
+
+    server.once('message', message => {
+      expect(message).toEqual('PART #dallas')
+      done()
+    })
+
+    chat.part('#dallas')
   })
 
   describe('should handle multiple channels', () => {
