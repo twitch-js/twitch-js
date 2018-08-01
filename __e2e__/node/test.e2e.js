4f1317d566fb28b9f10c48580116cc3b5b7feb31
Marc Andrews <marcandrews@users.noreply.github.com>
2018-08-01 18:29:49 -0400

    Add E2E tests

---
+++ __e2e__/node/test.e2e.js
@@ -0,0 +1,30 @@
+import TwitchJs from '../../lib'
+
+describe('Node E2E', () => {
+  const token = process.env.TWITCH_TOKEN
+  const username = process.env.TWITCH_USERNAME
+  const channel = process.env.TWITCH_USERNAME
+  const message = process.env.TRAVIS_BUILD_NUMBER
+    ? `Travis CI E2E Build #${process.env.TRAVIS_BUILD_NUMBER}`
+    : `Local E2E ${new Date()}`
+
+  describe('Chat', () => {
+    test('should connect and send message', async () => {
+      const { chat } = new TwitchJs({ token, username })
+
+      await chat.connect()
+      await chat.join(channel)
+      await chat.say(channel, message)
+
+      chat.disconnect()
+    })
+  })
+
+  describe('Api', () => {
+    test('should get endpoint', async () => {
+      const { api } = new TwitchJs({ token, username })
+
+      await api.get('streams/featured')
+    })
+  })
+})
