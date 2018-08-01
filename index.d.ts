2d99c26d23d9198b04ca15f1873eb5797f6b0420
Evan Steinkerchner <esteinkerchner@gmail.com>
2018-08-01 18:18:43 -0400

    Added JSDoc comments

--- index.d.ts
+++ index.d.ts
@@ -22,18 +22,69 @@ export type Listener = (...args: any[]) => void;
 
 export class EventEmitter {
   static defaultMaxListeners: number;
-  static listenerCount(emitter: EventEmitter, type: string | number): number;
+  static listenerCount(emitter: EventEmitter, eventName: string | symbol): number;
 
+  /**
+   * By default EventEmitters will print a warning if more than 10 listeners are
+   * added for a particular event. This is a useful default that helps finding
+   * memory leaks. Obviously, not all events should be limited to just 10
+   * listeners. The emitter.setMaxListeners() method allows the limit to be
+   * modified for this specific EventEmitter instance. The value can be set to
+   * Infinity (or 0) to indicate an unlimited number of listeners.
+   */
   setMaxListeners(n: number): this;
-  emit(type: string | number, ...args: any[]): boolean;
-  addListener(type: string | number, listener: Listener): this;
-  on(type: string | number, listener: Listener): this;
-  once(type: string | number, listener: Listener): this;
-  removeListener(type: string | number, listener: Listener): this;
-  removeAllListeners(type?: string | number): this;
-  listeners(type: string | number): Listener[];
-  listenerCount(type: string | number): number;
-  emits(types: ReadonlyArray<string | number>, values: ReadonlyArray<any>): void;
+
+  /**
+   * Synchronously calls each of the listeners registered for the event named
+   * eventName, in the order they were registered, passing the supplied
+   * arguments to each.
+   * @returns true if the event had listeners, false otherwise
+   */
+  emit(eventName: string | symbol, ...args: any[]): boolean;
+
+  /**
+   * Adds the listener function to the end of the listeners array for the event
+   * named eventName. Alias for on.
+   */
+  addListener(eventName: string | symbol, listener: Listener): this;
+
+  /**
+   * Adds the listener function to the end of the listeners array for the event
+   * named eventName.
+   */
+  on(eventName: string | symbol, listener: Listener): this;
+
+  /**
+   * Adds a one-time listener function for the event named eventName. The next
+   * time eventName is triggered, this listener is removed and then invoked.
+   */
+  once(eventName: string | symbol, listener: Listener): this;
+
+  /**
+   * Removes the specified listener from the listener array for the event named
+   * eventName.
+   */
+  removeListener(eventName: string | symbol, listener: Listener): this;
+
+  /**
+   * Removes all listeners, or those of the specified eventName.
+   */
+  removeAllListeners(eventName?: string | symbol): this;
+
+  /**
+   * Returns a copy of the array of listeners for the event named eventName.
+   */
+  listeners(eventName: string | symbol): Listener[];
+
+  /**
+   * Returns the number of listeners listening to the event named eventName.
+   */
+  listenerCount(eventName: string | symbol): number;
+
+  /**
+   * Emits multiple events named eventNames[i] with the data of values[i].
+   */
+  emits(eventNames: ReadonlyArray<string | symbol>, values: ReadonlyArray<any>): void;
 }
 
 export interface ClientOptions {
@@ -79,19 +130,50 @@ export interface TagsCollection {
 }
 
 export interface Utilities {
+  /**
+   * Perform a Levenshtein distance calculation between two strings.
+   */
   levenshtein: (s1: string, s2: string, caseSensitive: boolean) => number;
   raffle: Raffle;
+  /**
+   * Count the number of symbols in a string of text.
+   */
   symbols: (line: string) => number;
+  /**
+   * Count the number of uppercase letters in a string of text.
+   */
   uppercase: (line: string) => number;
 }
 
 export interface Raffle {
+  /**
+   * Initialize a raffle for a channel. No need to call this manually, because
+   * it is automatically called for you at the beginning of each other method.
+   */
   init: (channel: string) => void;
+  /**
+   * Enter a user into the raffle on a channel.
+   */
   enter: (channel: string, username: string) => void;
+  /**
+   * Remove a user from the raffle on a channel.
+   */
   leave: (channel: string, username: string) => boolean;
+  /**
+   * Choose a winner for the raffle on a channel.
+   */
   pick: (channel: string) => string|null;
+  /**
+   * Clear/reset the pool of entries for the raffle on a channel.
+   */
   reset: (channel: string) => void;
+  /**
+   * Count the number of entries for the raffle on a channel.
+   */
   count: (channel: string) => number;
+  /**
+   * Check if a user is entered for the raffle on a channel.
+   */
   isParticipating: (channel: string, username: string) => boolean;
 }
 
@@ -129,53 +211,278 @@ export class Client extends EventEmitter {
   constructor(opts?: ClientOptions);
 
   /// Base Methods
+  /**
+   * Make a call to the Twitch.tv kraken api.
+   * See https://dev.twitch.tv/docs/v5/.
+   * @returns Promise(apiResponse)
+   */
   api(options?: ApiOptions, cb?: RequestCallback): Promise<any>;
+
+  /**
+   * Connect to server.
+   * @returns Promise([server, port])
+   */
+  connect(): Promise<[string, number]>;
+
+  /**
+   * Disconnect from server.
+   * @returns Promise([server, port])
+   */
+  disconnect(): Promise<[string, number]>;
+
+  /**
+   * Get the current channels.
+   */
   getChannels(): string[];
+
+  /**
+   * Get the current options.
+   */
   getOptions(): ClientOptions;
+
+  /**
+   * Get the current username.
+   */
   getUsername(): string;
+
+  /**
+   * Handle parsed chat server message. Do not call this manually unless you
+   * replace the onmessage handler on the WebSocket.
+   */
   handleMessage(message: Message): void;
+
+  /**
+   * NOT RECOMMENDED. Might not be accurate dure to race conditions. Use the
+   * chat event and check the user's mod property to tell if they are a mod or
+   * not.
+   * 
+   * Check if username is a moderator on a channel.
+   */
   isMod(channel: string, username: string): boolean;
+
+  /**
+   * Get the current state of the socket.
+   */
   readyState(): 'CONNECTING'|'OPEN'|'CLOSING'|'CLOSED';
 
   /// Commands
+  /**
+   * Send action message (/me <message>) on a channel.
+   * @returns Promise([channel, message])
+   */
   action(channel: string, message: string): Promise<[string, string]>;
+
+  /**
+   * Ban username on channel.
+   * @returns Promise([channel, username, reason])
+   */
   ban(channel: string, username: string, reason: string): Promise<[string, string, string]>;
+
+  /**
+   * Clear all messages on a channel.
+   * @returns Promise([channel])
+   */
   clear(channel: string): Promise<[string]>;
+
+  /**
+   * Change the color of your username.
+   * @returns Promise([color])
+   */
   color(newColor: string): Promise<[string]>;
+
+  /**
+   * Change the color of your username. Attempts to set the color to the first
+   * parameter, and sets as the second on failure. Useful for setting turbo-only
+   * colors with normal color fallbacks.
+   * @returns Promise([color])
+   */
   // tslint:disable-next-line unified-signatures
   color(newColorAttempt: string, newColorFallback: string): Promise<[string]>;
+
+  /**
+   * Run commercial on a channel for X seconds.
+   * @returns Promise([channel, seconds])
+   */
   commercial(channel: string, seconds: number): Promise<[string, number]>;
-  connect(): Promise<[string, number]>;
-  disconnect(): Promise<[string, number]>;
+  
+  /**
+   * Enable emote-only mode on a channel.
+   * @returns Promise([channel])
+   */
   emoteonly(channel: string): Promise<[string]>;
+
+  /**
+   * Disable emote-only mode on a channel.
+   * @returns Promise([channel])
+   */
   emoteonlyoff(channel: string): Promise<[string]>;
+
+  /**
+   * Enable followers-only mode on a channel. Alias of followersonly.
+   * @returns Promise([channel, minutes])
+   */
   followersmode(channel: string, minutes: number): Promise<[string, number]>;
+
+  /**
+   * Disable followers-only mode on a channel. Alias of followersonlyoff.
+   * @returns Promise([channel])
+   */
   followersmodeoff(channel: string): Promise<[string]>;
+
+  /**
+   * Enable followers-only mode on a channel.
+   * @returns Promise([channel, minutes])
+   */
   followersonly(channel: string, minutes: number): Promise<[string, number]>;
+
+  /**
+   * Disable followers-only mode on a channel.
+   * @returns Promise([channel])
+   */
   followersonlyoff(channel: string): Promise<[string]>;
+
+  /**
+   * Host a channel.
+   * @returns Promise([channel, target, hostsRemaining])
+   */
   host(channel: string, target: string): Promise<[string, string, number]>;
+
+  /**
+   * Join a channel.
+   * @returns Promise([channel])
+   */
   join(channel: string): Promise<[string]>;
+
+  /**
+   * Leave a channel. Alias of part.
+   * @returns Promise([channel])
+   */
   leave(channel: string): Promise<[string]>;
+
+  /**
+   * Mod username on channel.
+   * @returns Promise([channel, username ])
+   */
   mod(channel: string, username: string): Promise<[string, string]>;
+
+  /**
+   * Get list of mods on a channel.
+   * @returns Promise([channel])
+   */
   mods(channel: string): Promise<string[]>;
+
+  /**
+   * Leave a channel.
+   * @returns Promise([channel])
+   */
   part(channel: string): Promise<[string]>;
+
+  /**
+   * Send a ping to the server.
+   * @returns Promise([latency])
+   */
   ping(): Promise<[number]>;
+
+  /**
+   * Enable R9KBeta mode on a channel.
+   * @returns Promise([channel])
+   */
   r9kbeta(channel: string): Promise<[string]>;
+
+  /**
+   * Disable R9KBeta mode on a channel.
+   * @returns Promise([channel])
+   */
   r9kbetaoff(channel: string): Promise<[string]>;
+
+  /**
+   * Enable R9KBeta mode on a channel. Alias of r9kbeta.
+   * @returns Promise([channel])
+   */
   r9kmode(channel: string): Promise<[string]>;
+
+  /**
+   * Disable R9KBeta mode on a channel. Alias of r9kbetaoff.
+   * @returns Promise([channel])
+   */
   r9kmodeoff(channel: string): Promise<[string]>;
+
+  /**
+   * Send a raw message to the server.
+   * @returns Promise([message])
+   */
   raw(message: string): Promise<[string]>;
+
+  /**
+   * Send a message on a channel.
+   * @returns Promise([channel, message])
+   */
   say(channel: string, message: string): Promise<[string, string]>;
+
+  /**
+   * Enable slow mode on a channel.
+   * @returns Promise([channel])
+   */
   slow(channel: string, seconds: number): Promise<[string, number]>;
+
+  /**
+   * Enable slow mode on a channel. Alias of slow.
+   * @returns Promise([channel])
+   */
   slowmode(channel: string, seconds: number): Promise<[string, number]>;
+
+  /**
+   * Disable slow mode on a channel. Alias of slowoff.
+   * @returns Promise([channel])
+   */
   slowmodeoff(channel: string): Promise<[string]>;
+
+  /**
+   * Disable slow mode on a channel.
+   * @returns Promise([channel])
+   */
   slowoff(channel: string): Promise<[string]>;
+
+  /**
+   * Enable subscribers mode on a channel.
+   * @returns Promise([channel])
+   */
   subscribers(channel: string): Promise<[string]>;
+
+  /**
+   * Disable subscribers mode on a channel.
+   * @returns Promise([channel])
+   */
   subscribersoff(channel: string): Promise<[string]>;
+
+  /**
+   * Timeout username on channel for X seconds.
+   * @returns Promise([channel, username, seconds, reason])
+   */
   timeout(channel: string, username: string, seconds: number, reason: string): Promise<[string, string, number, string]>;
+  
+  /**
+   * Unban username on channel.
+   * @returns Promise([channel, username])
+   */
   unban(channel: string, username: string): Promise<[string, string]>;
+
+  /**
+   * End the current hosting.
+   * @returns Promise([channel])
+   */
   unhost(channel: string): Promise<[string]>;
+
+  /**
+   * Unmod username on channel.
+   * @returns Promise([channel, username])
+   */
   unmod(channel: string, username: string): Promise<[string, string]>;
+
+  /**
+   * Send a whisper message to a user.
+   * @returns Promise([username, message])
+   */
   whisper(username: string, message: string): Promise<[string, string]>;
 }
 
