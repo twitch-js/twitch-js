837ffbbcb448c2b5f184120f687f7a496ed06ec2
Alexandre B <contact@seldszar.fr>
2018-09-24 14:28:02 +0200

    Chore: add `isAnonymousUsername()` unit tests

--- src/Chat/utils/__tests__/index.test.js
+++ src/Chat/utils/__tests__/index.test.js
@@ -69,4 +69,18 @@ describe('Chat/utils', () => {
       expect(actual).toEqual(expected)
     })
   })
+
+  describe('isAnonymousUsername', () => {
+    test('should return true if anonymous', () => {
+      const actual = utils.isAnonymousUsername('justinfan12345')
+      const expected = true
+      expect(actual).toEqual(expected)
+    })
+
+    test('should return false otherwise', () => {
+      const actual = utils.isAnonymousUsername('lorem_ipsum')
+      const expected = false
+      expect(actual).toEqual(expected)
+    })
+  })
 })
