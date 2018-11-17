cd61ef07ec099e5b39f43cd41bebb34f88388b37
DBKynd <dbkynd@users.noreply.github.com>
2018-11-16 19:05:31 -0700

    add USERNOTICE event anongiftpaidupgrade (#179)

--- src/Chat/constants.js
+++ src/Chat/constants.js
@@ -146,6 +146,7 @@ export const NOTICE_MESSAGE_IDS = {
 
 // See https://dev.twitch.tv/docs/irc/tags#usernotice-twitch-tags.
 export const USER_NOTICE_MESSAGE_IDS = {
+  ANON_GIFT_PAID_UPGRADE: 'anongiftpaidupgrade',
   GIFT_PAID_UPGRADE: 'giftpaidupgrade',
   RAID: 'raid',
   RESUBSCRIPTION: 'resub',
@@ -178,6 +179,7 @@ export const EVENTS = {
   ERROR_ENCOUNTERED: 'ERROR_ENCOUNTERED',
   PARSE_ERROR_ENCOUNTERED: 'PARSE_ERROR_ENCOUNTERED',
 
+  ANON_GIFT_PAID_UPGRADE: 'ANON_GIFT_PAID_UPGRADE',
   GIFT_PAID_UPGRADE: 'GIFT_PAID_UPGRADE',
   RAID: 'RAID',
   RESUBSCRIPTION: 'RESUBSCRIPTION',
