1c5a96bd53c9461622ab5153004d0f8cbbf5e97d
Marc Andrews <marcandrews@users.noreply.github.com>
2018-11-27 23:19:32 -0500

    Refactor Queue

--- src/Chat/Queue.js
+++ src/Chat/Queue.js
@@ -1,54 +1,78 @@
 import BetterQueue from 'better-queue'
 import MemoryStore from 'better-queue-memory'
 import setImmediate from 'core-js/library/fn/set-immediate'
+import { get } from 'lodash'
 
 import * as constants from './constants'
 
-class Queue extends BetterQueue {
-  rateLimiter = 0
-  rateLimiterIntervalId
+const EMPTY_INTERVAL_ID = -1
 
-  constructor() {
-    // Instantiate Better Queue.
-    super(
+class Queue {
+  _q
+
+  _maxLength = 20
+  _length = 0
+
+  _tickInterval = 30000
+  _tickIntervalId = EMPTY_INTERVAL_ID
+
+  constructor({ maxLength = 20, tickInterval = 30000 } = {}) {
+    this._tickInterval = tickInterval
+    this._maxLength = maxLength
+
+    this._q = new BetterQueue(
       ({ fn }, cb) => {
         fn()
-        cb(this.rateLimiter)
+        cb()
       },
       {
         store: new MemoryStore(),
-        priority: ({ priority = 1 }, cb) => cb(null, priority),
         setImmediate,
-        // Process queue only when rate-limiter is less than 1.
-        precondition: cb => cb(null, this.rateLimiter < 1),
+        priority: this._handlePriority,
+        precondition: this._handlePrecondition,
         preconditionRetryTimeout: constants.QUEUE_TICK_RATE,
       },
     )
 
-    // Start rate-limiter burn down.
-    this.rateLimiterIntervalId = setInterval(
-      this.burnDownRateLimiter.bind(this),
-      constants.QUEUE_TICK_RATE,
-    )
+    this._q.on('drain', this._handleQueueDrained)
   }
 
-  incrementRateLimiter(weight) {
-    return () => {
-      this.rateLimiter += 1 / weight
-    }
-  }
-
-  burnDownRateLimiter() {
-    this.rateLimiter = Math.max(
-      this.rateLimiter - constants.QUEUE_BURNDOWN_RATE,
-      0,
-    )
-  }
-
-  push({ fn, priority, weight }) {
-    return super
+  push({ fn, priority }) {
+    return this._q
       .push({ fn, priority })
-      .on('accepted', this.incrementRateLimiter(weight))
+      .on('accepted', this._handleTaskAccepted)
+      .on('finish', this._handleTaskFinish)
+  }
+
+  _handlePriority = ({ priority = 1 }, cb) => cb(null, priority)
+
+  _handlePrecondition = cb => cb(null, this._length <= this._maxLength)
+
+  _handleTaskAccepted = () => {
+    if (this._tickIntervalId === EMPTY_INTERVAL_ID) {
+      this._tickIntervalId = setInterval(this._tick, this._tickInterval)
+    }
+
+    this._increaseLength()
+  }
+
+  _tick = () => (this._length = Math.max(0, this._length - this._maxLength))
+
+  _handleTaskFinish = () => {
+    this._decreaseLength()
+  }
+
+  _increaseLength = () => {
+    this._length = this._length + 1
+  }
+
+  _decreaseLength = () => {
+    this._length = Math.max(0, this._length - 1)
+  }
+
+  _handleQueueDrained = () => {
+    clearInterval(this._tickIntervalId)
+    this._tickIntervalId = EMPTY_INTERVAL_ID
   }
 }
 
