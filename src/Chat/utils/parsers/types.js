ad57cedebffc19b863e2879b9c1fd7c518665e47
Marc Andrews <marcandrews@users.noreply.github.com>
2018-12-08 11:36:56 -0500

    Parse badges

--- src/Chat/utils/parsers/types.js
+++ src/Chat/utils/parsers/types.js
@@ -35,24 +35,61 @@ const broadcasterLanguage = maybeLanguage => {
   return typeof maybeLanguage === 'string' ? maybeLanguage : undefined
 }
 
+const followersOnly = maybeFollowersOnly => {
+  const followersOnlyAsNumber = parseInt(maybeFollowersOnly, 10)
+
+  if (followersOnlyAsNumber === 0) {
+    return true
+  } else if (followersOnlyAsNumber > 0) {
+    return followersOnlyAsNumber
+  }
+
+  return false
+}
+
 const badges = maybeBadges => {
   /**
    * Badges tag
    * @typedef {Object} BadgesTag
    * @property {boolean} [admin]
    * @property {number} [bits]
+   * @property {number} [bitsLeader]
    * @property {boolean} [broadcaster]
    * @property {boolean} [globalMod]
    * @property {boolean} [moderator]
-   * @property {boolean} [subscriber]
+   * @property {boolean} [premium]
    * @property {boolean} [staff]
+   * @property {boolean} [subGifter]
+   * @property {number} [subscriber]
    * @property {boolean} [turbo]
+   * @property {boolean} [vip]
    * @see https://dev.twitch.tv/docs/irc/tags/#privmsg-twitch-tags
    */
   return typeof maybeBadges === 'string'
     ? maybeBadges.split(',').reduce((parsed, badge) => {
-        const [key, value] = badge.split('/')
-        return { ...parsed, [camelCase(key)]: parseInt(value, 10) }
+        const [rawKey, value] = badge.split('/')
+        const key = camelCase(rawKey)
+
+        switch (key) {
+          // Booleans
+          case 'admin':
+          case 'broadcaster':
+          case 'globalMod':
+          case 'moderator':
+          case 'premium':
+          case 'staff':
+          case 'subGifter':
+          case 'turbo':
+          case 'vip':
+            return { ...parsed, [key]: generalBoolean(value) }
+          // Numbers
+          case 'bits':
+          case 'bitsLeader':
+          case 'subscriber':
+            return { ...parsed, [key]: parseInt(value, 10) }
+          default:
+            return { ...parsed, [key]: value }
+        }
       }, {})
     : {}
 }
@@ -102,6 +139,7 @@ export {
   usernameFromPrefix,
   userType,
   broadcasterLanguage,
+  followersOnly,
   badges,
   emotes,
   emoteSets,
