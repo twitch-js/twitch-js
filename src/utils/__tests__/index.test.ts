64dadb90500fbf31c129e0d8dc56ae67ea3d746f
Victor Perin <me@victorperin.ninja>
2020-09-17 01:43:15 -0700

    Bugfix: add timeout to resolveOnEvent (#259)
    * Feature: allow resolveOnEvent to have a timeout
    
    * Feature: add a time limit to timeout function
    
    * Chore: Remmove unused import
    
    * Chore: using COMMAND_TIMEOUT constant instead of using a new one
    
    * Chore: using different event emitters for each tests for determinisability

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -1,4 +1,5 @@
 import * as utils from '../'
+import EventEmitter from 'eventemitter3'
 
 describe('utils', () => {
   describe('resolveAfter', () => {
@@ -6,6 +7,67 @@ describe('utils', () => {
       jest.useFakeTimers()
       utils.resolveAfter(100).then(() => done())
       jest.runOnlyPendingTimers()
+    })
+  })
+
+  describe('resolveOnEvent', () => {
+    beforeEach(jest.useFakeTimers)
+
+    afterEach(jest.restoreAllMocks)
+
+    it('should resolve promise on event', done => {
+      const emitter = new EventEmitter()
+
+      utils
+        .resolveOnEvent(emitter, 'event')
+        .then(result => expect(result).toEqual('value'))
+        .then(done)
+
+      emitter.emit('event', 'value')
+    })
+
+    it('should never reject if no timeout is passed', () => {
+      const emitter = new EventEmitter()
+      const resolveSpy = jest.fn()
+      const rejectSpy = jest.fn()
+
+      jest
+        .spyOn(global, 'Promise')
+        .mockImplementation(callback => callback(resolveSpy, rejectSpy))
+
+      utils.resolveOnEvent(emitter, 'event')
+      jest.runOnlyPendingTimers()
+
+      expect(resolveSpy).toBeCalledTimes(0)
+      expect(rejectSpy).toBeCalledTimes(0)
+    })
+
+    it('should reject with timemout', done => {
+      const emitter = new EventEmitter()
+
+      utils.resolveOnEvent(emitter, 'event', 10000).catch(error => {
+        expect(error).toEqual(new Error('no event emitted, timed out'))
+        done()
+      })
+
+      jest.runOnlyPendingTimers()
+    })
+
+    it('should only expire instance of timed out listener', async () => {
+      expect.assertions(2)
+      const emitter = new EventEmitter()
+
+      const firstPromise = utils.resolveOnEvent(emitter, 'event')
+      const secondPromise = utils.resolveOnEvent(emitter, 'event', 10000)
+
+      jest.advanceTimersByTime(10000)
+
+      emitter.emit('event', 'value')
+
+      await secondPromise.catch(error => expect(error).toBeTruthy())
+
+      const result = await firstPromise
+      expect(result).toEqual('value')
     })
   })
 
