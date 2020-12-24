2486e6cba05b428f595434824de516b31217e3c4
Suhun Han <ssut@ssut.me>
2020-12-24 11:41:16 +0900

    fix: add support for Web Worker

--- shims/form-data.js
+++ shims/form-data.js
@@ -1,1 +1,11 @@
-module.exports = window.FormData;
+let FormData;
+
+if (typeof global !== 'undefined' && typeof global.FormData !== 'undefined') {
+  FormData = global.FormData;
+} else if (typeof window !== 'undefined' && typeof window.FormData !== 'undefined') {
+  FormData = window.FormData;
+} else if (typeof self !== 'undefined' && typeof self.FormData !== 'undefined') {
+  FormData = self.FormData;
+}
+
+module.exports = FormData;
--- shims/node-fetch.js
+++ shims/node-fetch.js
@@ -1,3 +1,13 @@
 require('whatwg-fetch');
 
-module.exports = window.fetch;
+let fetch;
+
+if (typeof global !== 'undefined' && typeof global.fetch !== 'undefined') {
+  fetch = global.fetch;
+} else if (typeof window !== 'undefined' && typeof window.fetch !== 'undefined') {
+  fetch = window.fetch;
+} else if (typeof self !== 'undefined' && typeof self.fetch !== 'undefined') {
+  fetch = self.fetch;
+}
+
+module.exports = fetch;
